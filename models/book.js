c1758bd69c2b6d043261439a611b5e3efea895d4
Bermond Jon Batistiana <berjondev@gmail.com>
2020-12-14 19:59:28 -0800

    added documentation for book models

--- models/book.js
+++ models/book.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// book schema for the mongoDB
+// requires a title an author, a link, a description, an image, a googleId
+// required fields are and should be provided by the google book search API
 const bookSchema = new Schema({
   title: { type: String, required: true },
   subtitle: { type: String },
